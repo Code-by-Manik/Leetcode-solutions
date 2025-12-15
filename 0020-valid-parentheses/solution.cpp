e3b2c3f5314e8b415036e0553ecf990441ea5efa
Manik Aggarwal <manikagg16@gmail.com>
2025-12-15 07:37:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (38.12%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool isValid(string s) {
-
-        stack<char>st;
-
-        for(char i:s){
+        stack<char> st;
+        for(char i: s){
             if(i=='('||i=='{'||i=='['){
+
                 st.push(i);
-            }
 
+            }
             else{
-                if(st.empty()) return false;
-                char top=st.top();
-                st.pop();
 
-                if(i==')' && top!='('||i=='}' && top!='{'||i==']' && top!='[') return false;
+            if(st.empty()) return false;
+
+            char top=st.top();
+            st.pop();
+
+            if(i==')' && top!='(' || i=='}' && top!='{' || i==']' && top!='['){
+                return false;
             }
+
+            }
+
 
         }
+        if(st.empty()) return true;
 
-    return st.empty();
-
-
-        
+        return false;
     }
 };
