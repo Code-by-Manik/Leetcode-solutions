03220d61a9ff1fc5afd57dc244c394703b603f92
Manik Aggarwal <manikagg16@gmail.com>
2026-02-11 09:22:13 +0000

    Sync LeetCode submission Runtime - 1 ms (78.87%), Memory - 46.6 MB (81.43%)

--- 0026-remove-duplicates-from-sorted-array/solution.java
+++ 0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,26 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
-        Stack<Integer> st =new Stack<>();
-
-        
-
-        st.push(nums[nums.length-1]);
-
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]!=st.peek()){
-                st.push(nums[i]);
-            }
-        }
 
         int i=0;
+        int j=0;
 
-        while(st.size()!=0){
-            nums[i++]=st.peek();
-            st.pop();
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
+                  i++;
+
+                nums[i]=nums[j];
+            }
+            j++;
+  
         }
 
-        return i;
-
+        return i+1;
+        
     }
 }
