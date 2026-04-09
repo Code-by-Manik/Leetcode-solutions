b16afd2497894b961903da026c48ede4d105bf5a
Manik Aggarwal <manikagg16@gmail.com>
2026-04-09 07:12:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 60.9 MB (96.48%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -1,39 +1,28 @@
 class Solution {
+
+    static void rev(int [] arr,int i,int j){
+     
+
+        while(i<j){
+            int t=arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
+
+            i++;
+            j--;
+        }
+    }
+
     public void rotate(int[] nums, int k) {
 
         int size=nums.length;
-        ArrayList<Integer> arr1=new ArrayList<>();
-
-        ArrayList<Integer> arr2=new ArrayList<>();
-
 
         k=k%size;
 
-        for(int i=0;i<=size-k-1;i++){
 
-            arr1.add(nums[i]);
-
-        }
-
-        
-        for(int i=size-k;i<size;i++){
-
-            arr2.add(nums[i]);
-
-        }
-
-
-        for(int i=0;i<arr1.size();i++){
-            arr2.add(arr1.get(i));
-        }
-        
-
-        
-        for(int i=0;i<size;i++){
-
-           nums[i]=arr2.get(i);
-
-        }
+        rev(nums,0,size-1);
+        rev(nums,0,k-1);
+        rev(nums,k,size-1);
         
     }
 }
