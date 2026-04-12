bf03c2e0738408365e2714b3e69ba2f9dcfb8ee6
Manik Aggarwal <manikagg16@gmail.com>
2026-04-12 06:21:56 +0000

    Sync LeetCode submission Runtime - 3 ms (58.09%), Memory - 48.4 MB (62.23%)

---
+++ 4271-find-the-degree-of-each-vertex/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int ans[] = new int[matrix.length];
+         int k=0;
+        for(int i=0;i<matrix.length;i++){
+            int count=0;
+           
+            for(int j=0;j<matrix.length;j++){
+                if(matrix[i][j]==1) count++;
+
+            }
+
+            ans[k++] = count;
+
+        }
+    return ans;
+
+    }
+}
