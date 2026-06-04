2dcbf3833c01ee71f459bbc6b013cba7a43acda2
Manik Aggarwal <manikagg16@gmail.com>
2026-06-04 08:19:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (34.34%)

--- 0048-rotate-image/solution.java
+++ 0048-rotate-image/solution.java
@@ -1,27 +1,7 @@
 class Solution {
-
-    void rev(int arr[][]){
-
-          for(int i=0;i<arr.length;i++){
-        
-        int left=0;
-        int right=arr[0].length-1;
-     while(left<right){
-        int temp = arr[i][left];
-        arr[i][left] = arr[i][right];
-        arr[i][right] = temp;
-        left++;
-        right--;
-     }
-          }
-    }
     public void rotate(int[][] matrix) {
 
-        int r = matrix.length;
-        int c = matrix[0].length;
-
-       
-        for(int i=0;i<r;i++){
+        for(int i=0;i<matrix.length;i++){
             for(int j=0;j<i;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
@@ -29,10 +9,23 @@ class Solution {
             }
         }
 
-      
-            rev(matrix);
+    for(int i=0;i<matrix.length;i++){
+
+        int s=0;
+        int e=matrix[0].length-1;
+
+        while(s<e){
+             int temp = matrix[i][s];
+             matrix[i][s] = matrix[i][e];
+             matrix[i][e] = temp;
+
+             s++;
+             e--;
 
 
+        }
 
+    }
+        
     }
 }
