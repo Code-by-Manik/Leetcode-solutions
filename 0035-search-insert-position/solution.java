f9c85cc5e4729fc65ade47d9d657618a513d89fc
Manik Aggarwal <manikagg16@gmail.com>
2026-07-06 13:01:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.9 MB (12.19%)

--- 0035-search-insert-position/solution.java
+++ 0035-search-insert-position/solution.java
@@ -1,24 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-
-        int s=0;
-        int e=nums.length-1;
-        int mid=0;
+        int s = 0;
+        int e = nums.length-1;
 
         while(s<=e){
-        mid=s+(e-s)/2;
-            if(nums[mid]==target){
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid]<target){
-                s=mid+1;
-
+            else if(nums[mid] < target){
+                s = mid+1;
             }
             else{
-                e=mid-1;
+                e = mid - 1;
             }
-            
         }
         return s;
+        
     }
 }
