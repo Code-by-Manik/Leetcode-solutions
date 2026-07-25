a5c745f0218cb6120a8357130b9dfb4120aa6356
Manik Aggarwal <manikagg16@gmail.com>
2026-07-25 12:44:01 +0530

    Time: 2 ms (29.84%), Space: 42.7 MB (38.93%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -5,10 +5,29 @@ class Solution {
     public int maxProduct(int n) {
         String s = ""+n;
 
-        char str[] = s.toCharArray();
+        // char str[] = s.toCharArray();
 
-        Arrays.sort(str);
+        // Arrays.sort(str);
 
-        return (str[str.length-1]-'0')*(str[str.length-2]-'0');
+        // return (str[str.length-1]-'0')*(str[str.length-2]-'0');
+
+    int max=-1;
+    int smax = -1;
+
+    for(char i:s.toCharArray()){
+       
+
+        if(i > max){
+            smax=max;
+            max = i;
+            
+        }
+     else if(i<=max && i>=smax){
+            smax = i;
+        }
+    }
+
+    return (max-'0')*(smax-'0');
+
     }
 }
