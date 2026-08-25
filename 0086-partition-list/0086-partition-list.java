bfa53cba450bcfc048792e09b1df1c3eff8dd786
Manik Aggarwal <manikagg16@gmail.com>
2026-08-26 04:57:05 +0530

    Time: 0 ms (100%), Space: 43.7 MB (68.73%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head==null || head.next==null) return head;
+        boolean exist = false;
+        ListNode curr = head;
+        ListNode  s = new ListNode(0); 
+        ListNode hs = null;
+        boolean foundS = false;
+
+        ListNode  l = new ListNode(0);
+        ListNode hl=null;
+        boolean foundL = false;
+
+
+
+
+        while(curr!=null){
+            if(curr.val == x) exist = true;
+            if(curr.val < x){
+                if(!foundS){
+                    s.val = curr.val;
+                    foundS=true;
+                    hs = s;
+                }
+                else{
+                    ListNode next = new ListNode(curr.val);
+                    s.next = next;
+                    s=s.next;
+                }
+            }
+            if(curr.val>=x){
+
+                if(!foundL){
+                    l.val = curr.val;
+                    foundL=true;
+                    hl = l;
+                }
+                else{
+                    ListNode next = new ListNode(curr.val);
+                    l.next = next;
+                    l=l.next;
+                }
+            }
+
+
+            curr = curr.next;
+            
+        }
+
+        // if(!exist) return head;
+
+        if(!foundS) return hl;
+        if(!foundL) return hs;
+
+        s.next=hl;
+
+
+        return hs;
+
+
+        
+    }
+}
