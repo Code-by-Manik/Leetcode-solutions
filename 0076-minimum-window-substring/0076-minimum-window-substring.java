602e01789b8c5ea98216dc2536a0955bbcd5d82f
Manik Aggarwal <manikagg16@gmail.com>
2026-09-11 17:53:44 +0530

    Time: 300 ms (5%), Space: 48.6 MB (5.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,6 +1,5 @@
 class Solution {
     static boolean check(Map<Character,Integer> map){
-
         for(char i:map.keySet()){
             if(map.get(i)>0){
                 return false;
@@ -8,70 +7,40 @@ class Solution {
         }
 
         return true;
-
     }
     public String minWindow(String s, String t) {
         if(t.length() > s.length()) return "";
-        if(s.equals(t)) return s;
         Map<Character,Integer> map = new HashMap<>();
+
         for(char i:t.toCharArray()){
             map.put(i,map.getOrDefault(i,0)+1);
         }
+
         int i=0;
         int j=0;
         int min = s.length();
         String ans = "";
-        char str[] = s.toCharArray();
-
+        char nums[] = s.toCharArray();
         while(j<s.length()){
-            if(map.containsKey(str[j]))
-            map.put(str[j],map.get(str[j])-1);
-
-            // if(map.containsKey(str[j]) && map.get(str[j]) < 0){
-            //     while(map.get(str[j]) < 0){
-            //         if(map.containsKey(str[i]))
-            //             map.put(str[i],map.get(str[i])+1);
-            //         i++;
-            //     }
-            // }
-
+            if(map.containsKey(nums[j]))
+            map.put(nums[j],map.get(nums[j])-1);
 
             while(check(map)){
                 int len = j-i+1;
-                if(len <= min){
-                    min = len;
-                    ans = s.substring(i,j+1);
-
+                if(len <= min){ 
+                min = len;
+                ans = s.substring(i,j+1);
                 }
-                if(map.containsKey(str[i]))
-                    map.put(str[i],map.get(str[i])+1);
-                
+                if(map.containsKey(nums[i]))
+                map.put(nums[i],map.get(nums[i])+1);
+
                 i++;
             }
-
             j++;
 
         }
 
-        while(check(map) && i<j){
-            int len = j-i+1;
-                if(len < min){
-                    min = len;
-                    ans = s.substring(i,j);
-
-                }
-            if(map.containsKey(str[j]))
-                map.put(str[i],map.get(str[i])+1);
-            i++;
-
-
-        }
-
         return ans;
-
-
         
-
-
     }
 }
