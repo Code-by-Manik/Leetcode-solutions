c3170de6d930c41e2dcbc5302cc0c68f3527b2d1
Manik Aggarwal <manikagg16@gmail.com>
2026-09-11 05:45:36 +0530

    Time: 28 ms (22.26%), Space: 46.9 MB (26.28%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,44 +1,37 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        Map<Character,Integer> map = new HashMap<>();
+
+        char nums[] = s.toCharArray();
+        int max=0;
+        int max_freq=0;
+
         int i=0;
         int j=0;
-        int max=0;
-        int max_freq = 0;
-        char str[] = s.toCharArray();
-        Map<Character,Integer> map = new HashMap<>();
-        while(j<s.length()){
-            map.put(str[j],map.getOrDefault(str[j],0)+1);
-            for(char q:map.keySet()){
-                if(map.get(q) > max_freq){
-                     max_freq = map.get(q);
-                }
+
+        while(j<nums.length){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+
+            max_freq = Math.max(max_freq,map.get(nums[j]));
+
+            int rest=j-i+1 - max_freq;
+            while(rest>k){
+                char last=nums[i];
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(last) == 0)
+                    map.remove(last);
+                
+                i++;
+                rest=j-i+1 - max_freq;
             }
+
             int len = j-i+1;
-            int rest = len - max_freq;
-
-            while(rest > k){
-            char last = str[i];
-            map.put(str[i],map.get(str[i])-1);
-            if(map.get(last) == 0) map.remove(last);
-            i++;
-            max_freq = 0;
-            for(char q:map.keySet()){
-                if(map.get(q) > max_freq){
-                     max_freq = map.get(q);
-                }
-            }
-            len = j-i+1;
-            rest = len - max_freq;
-
-
-            }
-
-        
-        if(len > max) max = len;
-
-        j++;
-
+            max = Math.max(len,max);
+            j++;
         }
+
         return max;
+
+
     }
 }
