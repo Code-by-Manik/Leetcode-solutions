7f7fb46d2eb4dc64791bdd2dcad1cde688bae8e5
Manik Aggarwal <manikagg16@gmail.com>
2026-09-13 05:14:54 +0530

    Time: 303 ms (5%), Space: 48.4 MB (5.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -5,35 +5,34 @@ class Solution {
                 return false;
             }
         }
-
         return true;
     }
     public String minWindow(String s, String t) {
-        if(t.length() > s.length()) return "";
         Map<Character,Integer> map = new HashMap<>();
 
         for(char i:t.toCharArray()){
             map.put(i,map.getOrDefault(i,0)+1);
         }
 
-        int i=0;
         int j=0;
+        int i=0;
         int min = s.length();
         String ans = "";
         char nums[] = s.toCharArray();
         while(j<s.length()){
-            if(map.containsKey(nums[j]))
-            map.put(nums[j],map.get(nums[j])-1);
+            if(map.containsKey(nums[j])){
+                map.put(nums[j],map.get(nums[j])-1);
+            }
 
             while(check(map)){
-                int len = j-i+1;
-                if(len <= min){ 
-                min = len;
+                if(j-i+1 <= min){
+                min = Math.min(j-i+1,min);
                 ans = s.substring(i,j+1);
                 }
-                if(map.containsKey(nums[i]))
-                map.put(nums[i],map.get(nums[i])+1);
-
+                if(map.containsKey(nums[i])){
+                    map.put(nums[i],map.get(nums[i])+1);
+                }
+                
                 i++;
             }
             j++;
@@ -41,6 +40,8 @@ class Solution {
         }
 
         return ans;
-        
+
+
+
     }
 }
