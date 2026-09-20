2bc776650003610a7266de9c0c0f24c64a1f09f1
Manik Aggarwal <manikagg16@gmail.com>
2026-09-21 03:19:45 +0530

    Time: 1 ms (100%), Space: 44 MB (50.3%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,24 +1,6 @@
 class Solution {
-    static void rev(int arr[]){
-        int i=0;
-        int j=arr.length-1;
-
-        while(i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-            i++;
-            j--;
-        }
-
-    }
     public int reverseDegree(String s) {
         int freq[] = new int[26];
-
-        for(char i:s.toCharArray()){
-            freq[Math.abs(i - 'z')]++;
-        }
 
         int count=0;
 
